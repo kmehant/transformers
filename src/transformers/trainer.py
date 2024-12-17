8bbb65fe98b5d8c5eeaa44050a18edb6199fbe79
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2024-12-17 18:06:14 +0530

    sync is needed
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -2462,7 +2462,7 @@ class Trainer:
                     if self.state.is_world_process_zero:
                         print("do step")
                     params_before = [p.clone().detach() for p in model.parameters()]
-                    print(self.optimizer.optimizer.state_dict()['state'].values())
+                    print(self.optimizer.optimizer.state_dict())
                     self.optimizer.step()
                     res = True
                     for i, p in enumerate(model.parameters()):
