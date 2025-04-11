349efba7e801438d8ff1c1bda372f1ecd939cbd5
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-04-12 00:50:53 +0530

    debug
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -2348,7 +2348,7 @@ class Trainer:
             # In case of auto_find_batch_size=True
             # Remove FSDP wrapping from sub-models.
             self.model = unwrap_model(self.model, recursive=True)
-
+        torch.distributed.breakpoint()
         if delay_optimizer_creation:
             if use_accelerator_prepare:
                 # configure fsdp plugin for qlora if any
