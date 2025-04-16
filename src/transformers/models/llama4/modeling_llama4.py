fddd456a963d10bd4b7cb37f372370c4c53c55bc
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-04-16 15:05:26 +0530

    grad check
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- src/transformers/models/llama4/modeling_llama4.py
+++ src/transformers/models/llama4/modeling_llama4.py
@@ -610,7 +610,7 @@ class Llama4TextModel(Llama4PreTrainedModel):
         )
         self.norm = Llama4TextRMSNorm(config.hidden_size, eps=config.rms_norm_eps)
         self.rotary_emb = Llama4TextRotaryEmbedding(config=config)
-        # self.gradient_checkpointing = False
+        self.gradient_checkpointing = False
 
         # Initialize weights and apply final processing
         self.post_init()
