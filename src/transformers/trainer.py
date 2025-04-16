68128a7fcb615e2af977669d558f3e87b7678745
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-04-16 16:11:32 +0530

    remove profiler code
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -3779,7 +3779,6 @@ class Trainer:
             if self.accelerator.distributed_type == DistributedType.DEEPSPEED:
                 kwargs["scale_wrt_gas"] = False
             self.accelerator.backward(loss, **kwargs)
-            print(self.data)
 
             return loss.detach()
 
@@ -3798,29 +3797,30 @@ class Trainer:
             if num_items_in_batch is not None:
                 loss_kwargs["num_items_in_batch"] = num_items_in_batch
             inputs = {**inputs, **loss_kwargs}
-        def get_mem_MB():
-            torch.cuda.synchronize()
-            torch.cuda.empty_cache()
-            return torch.cuda.memory_allocated() / 1024 ** 2
-        self.data = {}
-        def make_forward_hook(name):
-            def forward_hook(module, input, output):
-                print(f"{name}_forward", get_mem_MB())
-                self.data[f"{name}_forward"] = get_mem_MB()
-            return forward_hook
+        # TO REMOVE
+        # def get_mem_MB():
+        #     torch.cuda.synchronize()
+        #     torch.cuda.empty_cache()
+        #     return torch.cuda.memory_allocated() / 1024 ** 2
+        # self.data = {}
+        # def make_forward_hook(name):
+        #     def forward_hook(module, input, output):
+        #         print(f"{name}_forward", get_mem_MB())
+        #         self.data[f"{name}_forward"] = get_mem_MB()
+        #     return forward_hook
 
-        def backward_hook(module, grad_input, grad_output):
-            print(f"{module} -- backward", get_mem_MB())
-            self.data[f"{module}_backward"] = get_mem_MB()
+        # def backward_hook(module, grad_input, grad_output):
+        #     print(f"{module} -- backward", get_mem_MB())
+        #     self.data[f"{module}_backward"] = get_mem_MB()
 
-        for name, module in model.named_modules():
-            module.register_forward_hook(make_forward_hook(name))
-            if "att" in name.lower():
-                if len(list(module.children())) == 0:  # only leaf modules
-                    module.register_full_backward_hook(backward_hook)
+        # for name, module in model.named_modules():
+        #     module.register_forward_hook(make_forward_hook(name))
+        #     if "att" in name.lower():
+        #         if len(list(module.children())) == 0:  # only leaf modules
+        #             module.register_full_backward_hook(backward_hook)
 
         outputs = model(**inputs)
-        print(self.data)
+
         # Save past state if it exists
         # TODO: this needs to be fixed and made cleaner later.
         if self.args.past_index >= 0:
