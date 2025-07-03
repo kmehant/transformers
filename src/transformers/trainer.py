3a66f99cffe09e5c2be18912e84ca38f0e00ccf3
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-07-03 20:48:00 +0530

    refactor: simplify model and optimizer prep
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- src/transformers/trainer.py
+++ src/transformers/trainer.py
@@ -2356,7 +2356,10 @@ class Trainer:
                 if self.use_apex:
                     model = self.accelerator.prepare(self.model)
                 else:
-                    model, self.optimizer = self.accelerator.prepare(self.model, self.optimizer)
+                    if delay_optimizer_creation:
+                        model = self.accelerator.prepare(self.model)
+                    else:
+                        model, self.optimizer = self.accelerator.prepare(self.model, self.optimizer)
             else:
                 # to handle cases wherein we pass "DummyScheduler" such as when it is specified in DeepSpeed config.
                 model, self.optimizer, self.lr_scheduler = self.accelerator.prepare(
