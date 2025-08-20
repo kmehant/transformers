485e65876102ef6ae7437401d115547a40720a40
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-08-20 21:37:28 +0530

    debug
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -464,8 +464,9 @@ def _end_ptr(tensor: torch.Tensor) -> int:
         try:
             stop = tensor.view(-1)[-1].data_ptr() + tensor.element_size()
         except:
-            print(tensor)
-            print(tensor.full_tensor())
+            # print(tensor)
+            print("ft", tensor.full_tensor())
+            print("ftview", tensor.full_tensor().view(-1))
             print(tensor.view(-1))
             print(tensor.view(-1)[-1])
             exit(1)
