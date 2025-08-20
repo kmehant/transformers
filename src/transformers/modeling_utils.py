b5eb0f881104c210dd5353482dc417eb474458b6
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2025-08-21 00:09:45 +0530

    debug
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- src/transformers/modeling_utils.py
+++ src/transformers/modeling_utils.py
@@ -468,7 +468,7 @@ def _end_ptr(tensor: torch.Tensor) -> int:
             print("ft", tensor.full_tensor().numel())
             print("ftview", tensor.full_tensor().view(-1).numel())
             # torch.distributed.breakpoint()
-            a = tensor.view(-1)
+            print(tensor.view(-1))
             print(tensor.view(-1)[-1])
             exit(1)
     else:
